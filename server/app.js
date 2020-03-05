f3f7415586a6d02948dd46c14a57cf83df20f3f6
CJ Roberts <cjroberts2013@gmail.com>
2020-03-05 00:14:02 -0600

    Connected to mongodb database and used dotenv to hide my apikey

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+mongoose.connect(process.env.MONGO_KEY);
+mongoose.connection.once("open", () => {
+	console.log("Connected to MongoDB");
+});
 
 app.use(
 	"/graphql",
